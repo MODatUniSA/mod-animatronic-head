402f3b81d2ad7f421aa8cf3d5b30fdc2686c1343
David Reece <dv.reece@gmail.com>
2018-01-14 18:36:23 +1030

    Add asyncio test object
    Used to ensure asyncio is behaving as expected.

---
+++ libs/asyncio/test_functions.py
@@ -0,0 +1,31 @@
+""" Helper functions for testing asyncio behaviour
+"""
+
+import time
+import asyncio
+import logging
+
+class AsyncioTestFunctions:
+    def __init__(self):
+        self._logger = logging.getLogger('asyncio_test_functions')
+        self._should_quit = False
+        self._logger.debug('Test functions object initted')
+
+    def stop(self):
+        self._should_quit = True
+
+    @asyncio.coroutine
+    def output_loop_count(self):
+        """ Coroutine to run while testing async behaviour to ensure we don't accidentally block the event loop. If count output is interrupted we have blocked async ops.
+        """
+        count = 1
+        last_time = time.time()
+
+        self._logger.debug('Outputting Asyncio Loop')
+
+        while not self._should_quit:
+            time_diff = time.time() - last_time
+            last_time = time.time()
+            self._logger.debug("Loop: {}. Time Diff: {:.2f}".format(count, time_diff))
+            yield from asyncio.sleep(1)
+            count += 1
