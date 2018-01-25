f44e78c9e8506b97bb5c8221abf3aaaf78d9e984
David Reece <dv.reece@gmail.com>
2018-01-25 15:22:53 +1030

    Improve debug output of null servo communicator

--- app/null_objects/null_servo_communicator.py
+++ app/null_objects/null_servo_communicator.py
@@ -3,13 +3,16 @@
 
 import logging
 
-from app.servo_control.servo_communicator import ServoCommunicator
+from app.servo_control.servo_positions import ServoPositions
 
 class NullServoCommunicator:
     def __init__(self):
         self._logger = logging.getLogger('null_servo_communicator')
 
     def move_to(self, positions, in_milliseconds=None):
+        if isinstance(positions, ServoPositions):
+            positions = positions.positions_str
+
         self._logger.debug("Moving Servo Positions: %s, in %s mS", positions, in_milliseconds)
 
     def stop_servos(self, servos):
