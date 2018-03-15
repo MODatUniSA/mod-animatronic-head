1c83d43ea904e3624258ad368cfe4bb2f11cacd2
David Reece <dv.reece@gmail.com>
2018-03-16 09:49:33 +1030

    Initial phoneme update

--- app/servo_control/phoneme_map.py
+++ app/servo_control/phoneme_map.py
@@ -1,7 +1,6 @@
 """ Maps phonemes to mouth shape positions. Phonemes/Positions taken from http://www.garycmartin.com/phoneme_examples.html and output of Papagayo.
 """
 
-# TODO: All phonemes currently match the prototype mouth. Update for new head.
 # IDEA: Take mapping from CSV
 
 from enum import Enum, auto
@@ -27,16 +26,16 @@ class PhonemeMap(ServoPositionMap):
         self['names'] = {}
         self['pins'] = {}
 
-        self['names'][Phonemes.CLOSED]  = { ServoMap.JAW : 1200, ServoMap.LIPS_UPPER : 1800, ServoMap.LIPS_LOWER : 1400, ServoMap.LIPS_LEFT : 1500, ServoMap.LIPS_RIGHT : 1500 }
-        self['names'][Phonemes.REST]  = { ServoMap.JAW : 1500, ServoMap.LIPS_UPPER : 1500, ServoMap.LIPS_LOWER : 1500, ServoMap.LIPS_LEFT : 1500, ServoMap.LIPS_RIGHT : 1500 }
-        self['names'][Phonemes.AI]    = { ServoMap.JAW : 2000, ServoMap.LIPS_UPPER : 1500, ServoMap.LIPS_LOWER : 1800, ServoMap.LIPS_LEFT : 1500, ServoMap.LIPS_RIGHT : 1500 }
-        self['names'][Phonemes.ETC]   = { ServoMap.JAW : 1500, ServoMap.LIPS_UPPER : 1500, ServoMap.LIPS_LOWER : 1700, ServoMap.LIPS_LEFT : 1700, ServoMap.LIPS_RIGHT : 1300 }
-        self['names'][Phonemes.E]     = { ServoMap.JAW : 1800, ServoMap.LIPS_UPPER : 1500, ServoMap.LIPS_LOWER : 1800, ServoMap.LIPS_LEFT : 1700, ServoMap.LIPS_RIGHT : 1300 }
-        self['names'][Phonemes.O]     = { ServoMap.JAW : 2000, ServoMap.LIPS_UPPER : 1300, ServoMap.LIPS_LOWER : 1700, ServoMap.LIPS_LEFT : 1300, ServoMap.LIPS_RIGHT : 1700 }
-        self['names'][Phonemes.U]     = { ServoMap.JAW : 1700, ServoMap.LIPS_UPPER : 1300, ServoMap.LIPS_LOWER : 1900, ServoMap.LIPS_LEFT : 1300, ServoMap.LIPS_RIGHT : 1700 }
+        self['names'][Phonemes.CLOSED]  = { ServoMap.JAW : 2200, ServoMap.LIPS_UPPER : 1363, ServoMap.LIPS_LOWER : 1363, ServoMap.LIPS_LEFT : 800, ServoMap.LIPS_RIGHT : 2200 }
+        self['names'][Phonemes.REST]  = { ServoMap.JAW : 2200, ServoMap.LIPS_UPPER : 1363, ServoMap.LIPS_LOWER : 1363, ServoMap.LIPS_LEFT : 800, ServoMap.LIPS_RIGHT : 2200 }
+        self['names'][Phonemes.AI]    = { ServoMap.JAW : 800, ServoMap.LIPS_UPPER : 2200, ServoMap.LIPS_LOWER : 2200, ServoMap.LIPS_LEFT : 800, ServoMap.LIPS_RIGHT : 2200 }
+        self['names'][Phonemes.ETC]   = { ServoMap.JAW : 1024, ServoMap.LIPS_UPPER : 2116, ServoMap.LIPS_LOWER : 2123, ServoMap.LIPS_LEFT : 1260, ServoMap.LIPS_RIGHT : 1730 }
+        self['names'][Phonemes.E]     = { ServoMap.JAW : 1024, ServoMap.LIPS_UPPER : 2116, ServoMap.LIPS_LOWER : 2123, ServoMap.LIPS_LEFT : 1260, ServoMap.LIPS_RIGHT : 1730 }
+        self['names'][Phonemes.O]     = { ServoMap.JAW : 800, ServoMap.LIPS_UPPER : 2200, ServoMap.LIPS_LOWER : 2200, ServoMap.LIPS_LEFT : 2200, ServoMap.LIPS_RIGHT : 800 }
+        self['names'][Phonemes.U]     = { ServoMap.JAW : 2200, ServoMap.LIPS_UPPER : 2087, ServoMap.LIPS_LOWER : 2087, ServoMap.LIPS_LEFT : 1018, ServoMap.LIPS_RIGHT : 1981 }
         self['names'][Phonemes.L]     = { ServoMap.JAW : 1500, ServoMap.LIPS_UPPER : 1200, ServoMap.LIPS_LOWER : 1700, ServoMap.LIPS_LEFT : 1300, ServoMap.LIPS_RIGHT : 1700 }
         self['names'][Phonemes.FV]    = { ServoMap.JAW : 1200, ServoMap.LIPS_UPPER : 1500, ServoMap.LIPS_LOWER : 1500, ServoMap.LIPS_LEFT : 1500, ServoMap.LIPS_RIGHT : 1500 }
-        self['names'][Phonemes.MBP]   = { ServoMap.JAW : 1200, ServoMap.LIPS_UPPER : 1800, ServoMap.LIPS_LOWER : 1400, ServoMap.LIPS_LEFT : 1500, ServoMap.LIPS_RIGHT : 1500 }
-        self['names'][Phonemes.WQ]    = { ServoMap.JAW : 1200, ServoMap.LIPS_UPPER : 1800, ServoMap.LIPS_LOWER : 1400, ServoMap.LIPS_LEFT : 1300, ServoMap.LIPS_RIGHT : 1700 }
+        self['names'][Phonemes.MBP]   = { ServoMap.JAW : 2183, ServoMap.LIPS_UPPER : 800, ServoMap.LIPS_LOWER : 800, ServoMap.LIPS_LEFT : 1587, ServoMap.LIPS_RIGHT : 1412 }
+        self['names'][Phonemes.WQ]    = { ServoMap.JAW : 2200, ServoMap.LIPS_UPPER : 800, ServoMap.LIPS_LOWER : 800, ServoMap.LIPS_LEFT : 2200, ServoMap.LIPS_RIGHT : 800 }
 
         self['pins'] = type(self).mapping_to_servo_positions(self['names'])
