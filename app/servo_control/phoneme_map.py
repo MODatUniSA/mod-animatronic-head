02c199630b81bd0df81ea64241763a581be429e3
David Reece <dv.reece@gmail.com>
2018-04-13 14:44:55 +0930

    Widen mouth for phonemes

--- app/servo_control/phoneme_map.py
+++ app/servo_control/phoneme_map.py
@@ -28,9 +28,9 @@ class PhonemeMap(ServoPositionMap):
 
         self['names'][Phonemes.CLOSED]  = { ServoMap.JAW : 2200, ServoMap.LIPS_UPPER : 1363, ServoMap.LIPS_LOWER : 1363, ServoMap.LIPS_LEFT : 800, ServoMap.LIPS_RIGHT : 2200 }
         self['names'][Phonemes.REST]  = { ServoMap.JAW : 2200, ServoMap.LIPS_UPPER : 1363, ServoMap.LIPS_LOWER : 1363, ServoMap.LIPS_LEFT : 800, ServoMap.LIPS_RIGHT : 2200 }
-        self['names'][Phonemes.AI]    = { ServoMap.JAW : 1000, ServoMap.LIPS_UPPER : 2199, ServoMap.LIPS_LOWER : 2199, ServoMap.LIPS_LEFT : 1610, ServoMap.LIPS_RIGHT : 1389 }
-        self['names'][Phonemes.ETC]   = { ServoMap.JAW : 1832, ServoMap.LIPS_UPPER : 2196, ServoMap.LIPS_LOWER : 2196, ServoMap.LIPS_LEFT : 1244, ServoMap.LIPS_RIGHT : 1755 }
-        self['names'][Phonemes.E]     = { ServoMap.JAW : 1832, ServoMap.LIPS_UPPER : 2196, ServoMap.LIPS_LOWER : 2196, ServoMap.LIPS_LEFT : 1244, ServoMap.LIPS_RIGHT : 1755 }
+        self['names'][Phonemes.AI]    = { ServoMap.JAW : 1000, ServoMap.LIPS_UPPER : 2199, ServoMap.LIPS_LOWER : 2199, ServoMap.LIPS_LEFT : 800, ServoMap.LIPS_RIGHT : 2200 }
+        self['names'][Phonemes.ETC]   = { ServoMap.JAW : 1832, ServoMap.LIPS_UPPER : 2196, ServoMap.LIPS_LOWER : 2196, ServoMap.LIPS_LEFT : 800, ServoMap.LIPS_RIGHT : 2200 }
+        self['names'][Phonemes.E]     = { ServoMap.JAW : 1832, ServoMap.LIPS_UPPER : 2196, ServoMap.LIPS_LOWER : 2196, ServoMap.LIPS_LEFT : 800, ServoMap.LIPS_RIGHT : 2200 }
         self['names'][Phonemes.O]     = { ServoMap.JAW : 1000, ServoMap.LIPS_UPPER : 2130, ServoMap.LIPS_LOWER : 2130, ServoMap.LIPS_LEFT : 1916, ServoMap.LIPS_RIGHT : 1083 }
         self['names'][Phonemes.U]     = { ServoMap.JAW : 1859, ServoMap.LIPS_UPPER : 1024, ServoMap.LIPS_LOWER : 1024, ServoMap.LIPS_LEFT : 2112, ServoMap.LIPS_RIGHT : 887 }
         self['names'][Phonemes.L]     = { ServoMap.JAW : 1190, ServoMap.LIPS_UPPER : 2105, ServoMap.LIPS_LOWER : 2105, ServoMap.LIPS_LEFT : 1014, ServoMap.LIPS_RIGHT : 1985 }
