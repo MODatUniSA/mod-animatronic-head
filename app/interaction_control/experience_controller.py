9297897da8cf3ddf9d5eb3f563b7ece0187bd081
David Reece <dv.reece@gmail.com>
2018-04-14 16:47:22 +0930

    Add handling for interrupted activating/deactivating

--- app/interaction_control/experience_controller.py
+++ app/interaction_control/experience_controller.py
@@ -16,7 +16,14 @@ from app.interaction_control.interaction_type import InteractionType
 from app.interaction_control.interaction_loop import InteractionLoop
 
 class ExperienceController:
-    states = ['idle', 'activating', 'active', 'deactivating']
+    states = [
+        'idle',
+        'activating',
+        'interrupted_activating',
+        'active',
+        'deactivating',
+        'interrupted_deactivating',
+    ]
 
     def __init__(self, interaction_loop_executor, user_detector):
         self._logger = logging.getLogger('experience_controller')
@@ -42,6 +49,9 @@ class ExperienceController:
     # INTERACTION EXECUTION
     # =========================================================================
 
+    # REVISE: Intearaction execution methods all almost identical. Could probably have a single
+    #           method and a map to determine which interactions to execute
+
     def _execute_idle(self):
         """ Executes the idle part of the current interaction loop
         """
@@ -62,14 +72,15 @@ class ExperienceController:
         self._logger.info("Executing Activating State")
         self._executor.queue_execution(InteractionType.ACTIVATING, interrupt)
 
-    def _execute_activating_from_deactivating(self):
+    def _execute_interrupted_activating(self):
         """ Execute the activating interaction from the deactivating state
         """
 
         if self._should_quit:
             return
 
-        self._execute_activating(interrupt=False)
+        self._logger.info("Executing Interrupted Activating State")
+        self._executor.queue_execution(InteractionType.INTERRUPTED_ACTIVATING, True)
 
     def _execute_active(self):
         if self._should_quit:
@@ -83,16 +94,14 @@ class ExperienceController:
             return
 
         self._logger.info("Executing Deactivating State")
-        self._executor.queue_execution(InteractionType.DEACTIVATING,True)
+        self._executor.queue_execution(InteractionType.DEACTIVATING)
 
-    # IDEA: Could have a concept of interrupted deactivating
-
-    def _execute_deactivating_from_activating(self):
+    def _execute_interrupted_deactivating(self):
         if self._should_quit:
             return
 
-        #TODO: Handle this differently if needed
-        self._execute_deactivating()
+        self._logger.info("Executing Interrupted Deactivating State")
+        self._executor.queue_execution(InteractionType.INTERRUPTED_DEACTIVATING, True)
 
     # INTERNAL HELPERS
     # =========================================================================
@@ -105,21 +114,64 @@ class ExperienceController:
         # return InteractionLoop('resources/interaction_loops/skin_loop.csv')
         # return InteractionLoop('resources/interaction_loops/now_that_youre_here_loop.csv')
         # return InteractionLoop('resources/interaction_loops/demo_loop.csv')
-        # return InteractionLoop('resources/interaction_loops/thursday_test_loop_1.csv')
-        return InteractionLoop('resources/interaction_loops/all_interactions_loop.csv')
+        return InteractionLoop('resources/interaction_loops/thursday_test_loop_1.csv')
+        # return InteractionLoop('resources/interaction_loops/all_interactions_loop.csv')
+        # return InteractionLoop('resources/interaction_loops/interrupt_test.csv')
 
     def _build_state_machine(self):
         """ Builds the state machine and adds all state transitions
         """
-        # REVISE: ignoring invalid triggers. May want to check in caller if transition valid.
+
+        # REVISE: Each state now has its own handling, could probably just call builtin machine
+        #           callbacks (on_enter_idle, etc.) to handle transitions, rather than using after
+        #           transition callbacks
+
         self._machine = Machine(model=self, states=type(self).states, initial='idle', ignore_invalid_triggers=True)
-        self._machine.add_transition('complete_idle', 'idle', 'idle', after='_execute_idle')
-        self._machine.add_transition('activate', 'idle', 'activating', after='_execute_activating')
-        self._machine.add_transition('activate', 'deactivating', 'activating', after='_execute_activating_from_deactivating')
-        self._machine.add_transition('complete_activation', 'activating', 'active', after='_execute_active')
-        self._machine.add_transition('deactivate', 'active', 'deactivating', after='_execute_deactivating')
-        self._machine.add_transition('deactivate', 'activating', 'deactivating', after='_execute_deactivating_from_activating')
-        self._machine.add_transition('complete_deactivation', 'deactivating', 'idle', after='_execute_idle')
+        self._machine.add_transition(
+            trigger ='complete_idle',
+            source  ='idle',
+            dest    ='idle',
+            after   ='_execute_idle'
+        )
+        self._machine.add_transition(
+            trigger ='activate',
+            source  ='idle',
+            dest    ='activating',
+            after   ='_execute_activating'
+        )
+        self._machine.add_transition(
+            trigger ='activate',
+            source  =['deactivating', 'interrupted_deactivating'],
+            dest    ='interrupted_activating',
+            after   ='_execute_interrupted_activating'
+        )
+        self._machine.add_transition(
+            trigger ='complete_activation',
+            source  =['activating', 'interrupted_activating'],
+            dest    ='active',
+            after   ='_execute_active'
+        )
+        self._machine.add_transition(
+            trigger ='deactivate',
+            source  ='active',
+            dest    ='deactivating',
+            after   ='_execute_deactivating'
+        )
+        self._machine.add_transition(
+            trigger ='deactivate_interrupt',
+            source  =['activating', 'active'],
+            dest    ='interrupted_deactivating',
+            after   ='_execute_interrupted_deactivating'
+        )
+        self._machine.add_transition(
+            trigger ='complete_deactivation',
+            source  =['deactivating', 'interrupted_deactivating'],
+            dest    ='idle',
+            after   ='_execute_idle'
+        )
+
+    # CALLBACKS
+    # =========================================================================
 
     def _add_executor_callbacks(self):
         """ Sets callbacks on executor so it can tell us when execution has completed
@@ -135,4 +187,4 @@ class ExperienceController:
         """
 
         self._user_detector.add_first_user_entered_callback(self.activate)
-        self._user_detector.add_all_users_left_callback(self.deactivate)
+        self._user_detector.add_all_users_left_callback(self.deactivate_interrupt)
