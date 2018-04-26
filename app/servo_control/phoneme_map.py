265796709d274727eaccdb3e08d478dcc824a0c2
David Reece <dv.reece@gmail.com>
2018-04-26 13:11:19 +0930

    Remove jaw positions from phoneme map

--- app/servo_control/phoneme_map.py
+++ app/servo_control/phoneme_map.py
@@ -26,16 +26,16 @@ class PhonemeMap(ServoPositionMap):
         self['names'] = {}
         self['pins'] = {}
 
-        self['names'][Phonemes.CLOSED]  = { ServoMap.JAW : 2200, ServoMap.LIPS_UPPER : 800, ServoMap.LIPS_LOWER : 800, ServoMap.LIPS_LEFT : 800, ServoMap.LIPS_RIGHT : 2200 }
-        self['names'][Phonemes.REST]  = { ServoMap.JAW : 2200, ServoMap.LIPS_UPPER : 800, ServoMap.LIPS_LOWER : 800, ServoMap.LIPS_LEFT : 800, ServoMap.LIPS_RIGHT : 2200 }
-        self['names'][Phonemes.AI]    = { ServoMap.JAW : 1000, ServoMap.LIPS_UPPER : 2199, ServoMap.LIPS_LOWER : 2199, ServoMap.LIPS_LEFT : 800, ServoMap.LIPS_RIGHT : 2200 }
-        self['names'][Phonemes.ETC]   = { ServoMap.JAW : 1832, ServoMap.LIPS_UPPER : 2196, ServoMap.LIPS_LOWER : 2196, ServoMap.LIPS_LEFT : 800, ServoMap.LIPS_RIGHT : 2200 }
-        self['names'][Phonemes.E]     = { ServoMap.JAW : 1832, ServoMap.LIPS_UPPER : 2196, ServoMap.LIPS_LOWER : 2196, ServoMap.LIPS_LEFT : 800, ServoMap.LIPS_RIGHT : 2200 }
-        self['names'][Phonemes.O]     = { ServoMap.JAW : 1000, ServoMap.LIPS_UPPER : 2130, ServoMap.LIPS_LOWER : 2130, ServoMap.LIPS_LEFT : 1916, ServoMap.LIPS_RIGHT : 1083 }
-        self['names'][Phonemes.U]     = { ServoMap.JAW : 1859, ServoMap.LIPS_UPPER : 1024, ServoMap.LIPS_LOWER : 1024, ServoMap.LIPS_LEFT : 2112, ServoMap.LIPS_RIGHT : 887 }
-        self['names'][Phonemes.L]     = { ServoMap.JAW : 1190, ServoMap.LIPS_UPPER : 2105, ServoMap.LIPS_LOWER : 2105, ServoMap.LIPS_LEFT : 1014, ServoMap.LIPS_RIGHT : 1985 }
-        self['names'][Phonemes.FV]    = { ServoMap.JAW : 1663, ServoMap.LIPS_UPPER : 1260, ServoMap.LIPS_LOWER : 1260, ServoMap.LIPS_LEFT : 817, ServoMap.LIPS_RIGHT : 2182 }
-        self['names'][Phonemes.MBP]   = { ServoMap.JAW : 2200, ServoMap.LIPS_UPPER : 800, ServoMap.LIPS_LOWER : 800, ServoMap.LIPS_LEFT : 1039, ServoMap.LIPS_RIGHT : 1960 }
-        self['names'][Phonemes.WQ]    = { ServoMap.JAW : 2200, ServoMap.LIPS_UPPER : 1151, ServoMap.LIPS_LOWER : 1151, ServoMap.LIPS_LEFT : 2199, ServoMap.LIPS_RIGHT : 800 }
+        self['names'][Phonemes.CLOSED]  = { ServoMap.LIPS_UPPER : 800, ServoMap.LIPS_LOWER : 800, ServoMap.LIPS_LEFT : 800, ServoMap.LIPS_RIGHT : 2200 }
+        self['names'][Phonemes.REST]  = { ServoMap.LIPS_UPPER : 800, ServoMap.LIPS_LOWER : 800, ServoMap.LIPS_LEFT : 800, ServoMap.LIPS_RIGHT : 2200 }
+        self['names'][Phonemes.AI]    = { ServoMap.LIPS_UPPER : 2199, ServoMap.LIPS_LOWER : 2199, ServoMap.LIPS_LEFT : 800, ServoMap.LIPS_RIGHT : 2200 }
+        self['names'][Phonemes.ETC]   = { ServoMap.LIPS_UPPER : 2196, ServoMap.LIPS_LOWER : 2196, ServoMap.LIPS_LEFT : 800, ServoMap.LIPS_RIGHT : 2200 }
+        self['names'][Phonemes.E]     = { ServoMap.LIPS_UPPER : 2196, ServoMap.LIPS_LOWER : 2196, ServoMap.LIPS_LEFT : 800, ServoMap.LIPS_RIGHT : 2200 }
+        self['names'][Phonemes.O]     = { ServoMap.LIPS_UPPER : 2130, ServoMap.LIPS_LOWER : 2130, ServoMap.LIPS_LEFT : 1916, ServoMap.LIPS_RIGHT : 1083 }
+        self['names'][Phonemes.U]     = { ServoMap.LIPS_UPPER : 1024, ServoMap.LIPS_LOWER : 1024, ServoMap.LIPS_LEFT : 2112, ServoMap.LIPS_RIGHT : 887 }
+        self['names'][Phonemes.L]     = { ServoMap.LIPS_UPPER : 2105, ServoMap.LIPS_LOWER : 2105, ServoMap.LIPS_LEFT : 1014, ServoMap.LIPS_RIGHT : 1985 }
+        self['names'][Phonemes.FV]    = { ServoMap.LIPS_UPPER : 1260, ServoMap.LIPS_LOWER : 1260, ServoMap.LIPS_LEFT : 817, ServoMap.LIPS_RIGHT : 2182 }
+        self['names'][Phonemes.MBP]   = { ServoMap.LIPS_UPPER : 800, ServoMap.LIPS_LOWER : 800, ServoMap.LIPS_LEFT : 1039, ServoMap.LIPS_RIGHT : 1960 }
+        self['names'][Phonemes.WQ]    = { ServoMap.LIPS_UPPER : 1151, ServoMap.LIPS_LOWER : 1151, ServoMap.LIPS_LEFT : 2199, ServoMap.LIPS_RIGHT : 800 }
 
         self['pins'] = type(self).mapping_to_servo_positions(self['names'])
